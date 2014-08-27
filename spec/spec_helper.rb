875a6d25bf40ce98feabc1b74701a534f9468cd1
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-08-27 15:59:47 +0100

    Ordered links into il

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,9 @@ ENV["RACK_ENV"] = 'test'
 
 require File.join(File.dirname(__FILE__), '../bookmark.rb')
 require 'database_cleaner'
+require 'capybara/rspec'
+
+Capybara.app = Sinatra::Application.new
 
 RSpec.configure do |config|
 
@@ -17,5 +20,7 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
+
+
 
 end
