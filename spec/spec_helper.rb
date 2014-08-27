ffd1844afa865e4142ee2eb03ee23dc146b13c57
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-08-27 15:47:32 +0100

    Added database cleaner to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,21 @@
 ENV["RACK_ENV"] = 'test' 
 
 require File.join(File.dirname(__FILE__), '../bookmark.rb')
+require 'database_cleaner'
+
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
